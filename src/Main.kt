50e1409f9bc0fa864cefcf8169b46087b248363c
Andrii Zadorozhnyi <ketinngz@gmail.com>
2024-11-10 16:53:44 +0200

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,9 @@
+fun myFunction(name: String) {
+    println(name)
+}
+
+
+fun main() {
+    println("Hello world")
+    myFunction("Asshole")
+}
---
+++ src/SmartDevice.kt
@@ -0,0 +1,12 @@
+class SmartDevice {
+    fun turnOn(){
+        println("Smart device is turned on.")
+    }
+    fun turnOff(){
+        println("Smart device is turned off.")
+    }
+}
+
+fun main(){
+    val smartTvDevice = SmartDevice()
+}
